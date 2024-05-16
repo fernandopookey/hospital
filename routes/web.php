999ccc1fb4df4739f82c57530bc39d5f28f1e8b5
RiyanHidayatulloh <Riyanska05@gmail.com>
2024-05-16 10:03:52 +0700

    yan coba

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
+Route::get('/masuk', function () {
+    return view('masuk');
 });
